326171fbd22b1d479e3af1ae7006fc0cf4a16ea7
AfsaraSilviAmi <afsarasilvi44@gmail.com>
2026-03-07 00:20:14 +0600

    Did the basic html for home

---
+++ scripts-folder/home.js
@@ -0,0 +1,9 @@
+//loading ALl
+const loadAll = () =>{
+    const url = "https://phi-lab-server.vercel.app/api/v1/lab/issues";
+    fetch(url)
+    .then((res)=>res.json())
+    .then((data)=>console.log(data.data));
+}
+
+loadAll();
